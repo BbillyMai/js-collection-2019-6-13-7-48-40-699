39a630feeebb620e70b5cc321534ce5848e6da6a
BbillyMai <1363543881@qq.com>
2019-07-10 14:40:41 +0800

    finished

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,6 +3,10 @@
 function choose_even(collection) {
 
   //implement here
+  const condition = (number)=>{
+    return number%2 === 0;
+  }
+  return collection.filter(condition);
 }
 
 module.exports = choose_even;
--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,6 +3,10 @@
 function choose_multiples_of_three(collection) {
 
   //implement here
+  const condition = (number) => {
+    return number % 3 ===0;
+  }
+  return collection.filter(condition);
 }
 
 module.exports = choose_multiples_of_three;
